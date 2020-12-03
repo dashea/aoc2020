50ebb0d8d607def814404f4cfa4488068f660af1
David Shea <reallylongword@gmail.com>
2020-12-03 10:31:12 -0500

    Add day 3 solutions

---
+++ 3-1.hs
@@ -0,0 +1,41 @@
+import Control.Monad.Catch (Exception, MonadThrow, throwM)
+import Data.Vector (Vector, (!?), fromList)
+import Type.Reflection (Typeable)
+
+data AoCException = ParseError
+ deriving (Show, Typeable)
+
+instance Exception AoCException
+
+data TreeSquare = Empty | Tree
+
+type Forest = Vector (Vector TreeSquare)
+type Point = (Int, Int)
+
+parseTreeLine :: MonadThrow m => String -> m (Vector TreeSquare)
+parseTreeLine input = fromList <$> mapM parseChar input
+ where
+    parseChar '.' = return Empty
+    parseChar '#' = return Tree
+    parseChar _   = throwM ParseError
+
+checkPoint :: Forest -> Point -> Maybe TreeSquare
+-- treat each row as infinitely repeating
+-- returns nothing when past the bottom of the forest
+checkPoint forest (x, y) = forest !? y >>= (\r -> r !? (x `mod` length r))
+
+-- returns the number of trees hit
+travelSlope :: Forest -> (Point -> Point) -> Point -> Int
+travelSlope forest slope currentPoint =
+    let nextPoint = slope currentPoint
+     in case checkPoint forest nextPoint of
+          Nothing    -> 0
+          Just Empty -> travelSlope forest slope nextPoint
+          Just Tree  -> 1 + travelSlope forest slope nextPoint
+
+main :: IO ()
+main = do
+    forest <- fromList <$> (getContents >>= mapM parseTreeLine . lines)
+    let startingPoint = (0, 0)
+        slope (x, y) = (x + 3, y + 1)
+    print $ travelSlope forest slope startingPoint
