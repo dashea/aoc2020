2a6644a74831849653198105909bd25b87bd5ef2
David Shea <reallylongword@gmail.com>
2020-12-10 14:30:35 -0500

    Add an optimization for sequences of all 1's

--- 10-2.hs
+++ 10-2.hs
@@ -10,6 +10,12 @@ allPossibilities (x1:x2:x3:xs) =
           else noSkip
 allPossibilities input = [input]
 
+possibilitiesLength :: [(Int, Int)] -> Int
+possibilitiesLength input =
+    let possibleOnes = 0:1:1:2:zipWith ((-) . (*2)) (drop 3 possibleOnes) possibleOnes
+        onesLen = possibleOnes !! length input
+     in if all ((== 1) . fst) input then onesLen else length (allPossibilities (map snd input))
+
 diffs :: [Int] -> [Int]
 diffs (x1:x2:xs) = (x2 - x1):diffs (x2:xs)
 diffs _ = []
@@ -20,5 +26,5 @@ main = do
     let adapterChain = sort (0:input)
 
     let diffPairs = zip (diffs adapterChain ++ [3]) adapterChain
-    let splitOnThrees = map (map snd) $ (split . dropFinalBlank . keepDelimsR . whenElt) ((== 3) . fst) diffPairs
-    print $ product $ map (length . allPossibilities) splitOnThrees
+    let splitOnThrees = (split . dropFinalBlank . keepDelimsR . whenElt) ((== 3) . fst) diffPairs
+    print $ product $ map possibilitiesLength splitOnThrees
