6332d3ed3ed9f1a104b8df1b6356401a7c973e7a
David Shea <reallylongword@gmail.com>
2020-12-10 13:29:36 -0500

    Just sort the adapter list
    They have to go in order, it either works or it doesn't.

--- 10-1.hs
+++ 10-1.hs
@@ -1,36 +1,20 @@
-import qualified Data.IntSet as IS
-import           Data.IntSet (IntSet)
-import           Data.Maybe (mapMaybe)
+import           Data.List (sort)
 
-tryAdapter :: Int -> IntSet -> Maybe [Int]
-tryAdapter currentJoltage adapters =
-    let possibleJoltages = map ($ currentJoltage) [(+1), (+2), (+3)]
-        possibleAdapters = filter (`IS.member` adapters) possibleJoltages
-        possibleResults = mapMaybe tryJolt possibleAdapters
-     in if IS.null adapters
-          then Just [currentJoltage]
-          else case possibleResults of
-                 (x:_) -> Just x
-                 []    -> Nothing
- where
-    tryJolt :: Int -> Maybe [Int]
-    tryJolt jolt = (currentJoltage:) <$> tryAdapter jolt (IS.delete jolt adapters)
-
-countOnesAndThrees :: (Int, Int) -> [Int] -> (Int, Int)
+countOnesAndThrees :: MonadFail m => (Int, Int) -> [Int] -> m (Int, Int)
 countOnesAndThrees (ones, threes) (x1:x2:xs) =
     case x2 - x1 of
       1 -> countOnesAndThrees (ones+1, threes) (x2:xs)
+      2 -> countOnesAndThrees (ones, threes) (x2:xs)
       3 -> countOnesAndThrees (ones, threes+1) (x2:xs)
-      _ -> countOnesAndThrees (ones, threes) (x2:xs)
-countOnesAndThrees acc _ = acc
+      x -> fail $ "Invalid transition: " ++ show x
+countOnesAndThrees acc _ = return acc
 
 main :: IO ()
 main = do
     input <- map read . lines <$> getContents
-    let adapters = IS.fromList input
 
-    adapterChain <- maybe (fail "no answer found") return $ tryAdapter 0 adapters
-    let (ones, threes) = countOnesAndThrees (0, 0) adapterChain
+    let adapterChain = sort (0:input)
+    (ones, threes) <- countOnesAndThrees (0, 0) adapterChain
 
     -- add one to threes for the difference between the last adapter and the device
     print $ ones * (threes + 1)
--- 10-2.hs
+++ 10-2.hs
@@ -1,21 +1,5 @@
-import qualified Data.IntSet as IS
-import           Data.IntSet (IntSet)
+import           Data.List (sort)
 import           Data.List.Split (dropFinalBlank, keepDelimsR, split, whenElt)
-import           Data.Maybe (mapMaybe)
-
-tryAdapter :: Int -> IntSet -> Maybe [Int]
-tryAdapter currentJoltage adapters =
-    let possibleJoltages = map ($ currentJoltage) [(+1), (+2), (+3)]
-        possibleAdapters = filter (`IS.member` adapters) possibleJoltages
-        possibleResults = mapMaybe tryJolt possibleAdapters
-     in if IS.null adapters
-          then Just [currentJoltage]
-          else case possibleResults of
-                 (x:_) -> Just x
-                 []    -> Nothing
- where
-    tryJolt :: Int -> Maybe [Int]
-    tryJolt jolt = (currentJoltage:) <$> tryAdapter jolt (IS.delete jolt adapters)
 
 allPossibilities :: [Int] -> [[Int]]
 allPossibilities (x1:x2:x3:xs) =
@@ -33,9 +17,7 @@ diffs _ = []
 main :: IO ()
 main = do
     input <- map read . lines <$> getContents
-    let adapters = IS.fromList input
-
-    adapterChain <- maybe (fail "no answer found") return $ tryAdapter 0 adapters
+    let adapterChain = sort (0:input)
 
     let diffPairs = zip (diffs adapterChain ++ [3]) adapterChain
     let splitOnThrees = map (map snd) $ (split . dropFinalBlank . keepDelimsR . whenElt) ((== 3) . fst) diffPairs
