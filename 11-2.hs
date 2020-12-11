c6f1beb097c52e6e1c0f4f6716feaa92029e5038
David Shea <reallylongword@gmail.com>
2020-12-11 11:42:33 -0500

    Add day 11 answers

---
+++ 11-2.hs
@@ -0,0 +1,64 @@
+import           Data.Bifunctor (bimap)
+import           Data.Maybe (mapMaybe)
+import qualified Data.Vector as V
+import           Data.Vector (Vector, (!?))
+
+data Square = Floor | Seat Bool
+ deriving (Eq)
+
+type Room = Vector (Vector Square)
+type Point = (Int, Int)
+
+getSquare :: Room -> Point -> Maybe Square
+getSquare r (x, y) = r !? y >>= (!? x)
+
+doConway :: Room -> Room
+doConway r = V.imap doRow r
+ where
+    doRow :: Int -> Vector Square -> Vector Square
+    doRow y = V.imap (doSquare y)
+
+    doSquare :: Int -> Int -> Square -> Square
+    doSquare _ _ Floor = Floor
+    doSquare y x s@(Seat _) =
+        let directions = [(subtract 1, id), (subtract 1, subtract 1), (id, subtract 1), ((+1), subtract 1),
+                          ((+1), id), ((+1), (+1)), (id, (+1)), (subtract 1, (+1))]
+            adjacents  = mapMaybe (visibleSeat (x, y)) directions
+            occupied   = length $ filter (== Seat True) $ mapMaybe (getSquare r) adjacents
+         in if occupied == 0
+              then Seat True
+              else if occupied >= 5
+                then Seat False
+                else s
+
+    visibleSeat :: Point -> (Int -> Int, Int -> Int) -> Maybe Point
+    visibleSeat p dp =
+        let p' = uncurry bimap dp p
+         in case getSquare r p' of
+              Nothing -> Nothing
+              Just Floor -> visibleSeat p' dp
+              Just (Seat _) -> Just p'
+
+runUntilStable :: Room -> Room
+runUntilStable room =
+    let room' = doConway room
+     in if room == room'
+          then room'
+          else runUntilStable room'
+
+parseRoom :: MonadFail m => [String] -> m Room
+parseRoom input = V.fromList <$> mapM parseRow input
+ where
+    parseRow :: MonadFail m => String -> m (Vector Square)
+    parseRow input' =  V.fromList <$> mapM parseSquare input'
+
+    parseSquare :: MonadFail m => Char -> m Square
+    parseSquare '.' = return Floor
+    parseSquare 'L' = return (Seat False)
+    parseSquare '#' = return (Seat True)
+    parseSquare x   = fail $ "Invalid character: " ++ show x
+
+main :: IO ()
+main = do
+    input <- (lines <$> getContents) >>= parseRoom
+    print $ V.length $ V.filter (== Seat True) $ V.concat $ V.toList $ runUntilStable input
