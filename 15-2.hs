d6b94e3b62831ad16df1b5a0ecf4514887eff14b
David Shea <reallylongword@gmail.com>
2020-12-15 10:16:43 -0500

    Add an even dumber 15-2 solution

---
+++ 15-2.hs
@@ -0,0 +1,25 @@
+import           Data.Conduit (ConduitT, (.|), await, runConduitPure, yield)
+import qualified Data.Conduit.Combinators as CC
+import qualified Data.HashMap.Lazy as HashMap
+import           Data.HashMap.Lazy (HashMap)
+
+takeTurn :: Monad m => Int -> Int -> HashMap Int Int -> HashMap Int Int -> ConduitT Int Int m ()
+takeTurn prevTurn prevInput prevMap curMap = do
+    input <- await
+    let turnNum = prevTurn + 1
+        output = case input of
+                   Just i -> i
+                   Nothing -> case HashMap.lookup prevInput prevMap of
+                                Nothing -> 0
+                                Just lastTurn -> prevTurn - lastTurn
+    yield output
+    takeTurn turnNum output curMap $ HashMap.insert output turnNum curMap
+
+main :: IO ()
+main = do
+    let startingNumbers = mapM_ yield [0,20,7,16,1,18,15]
+        pipeline = startingNumbers .|
+                   takeTurn 0 0 HashMap.empty HashMap.empty .|
+                   CC.take 30000000 .|
+                   (CC.drop 29999999 >> CC.head)
+    maybe (fail "Error getting answer") return (runConduitPure pipeline) >>= print
